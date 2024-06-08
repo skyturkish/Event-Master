33328298cb61ddc2b533b620c726757084c552ec
skyturkish <gokturk.acr2002@gmail.com>
2024-06-08 19:27:52 +0300

    Separate functions for event handling and button interactions
    - Simplified getMentionUsersString function to directly return the joined string.
    - Introduced helper functions (updateParticipantStatus, getUpdatedEvent, createEventEmbed, createButtons) for better code organization.
    - Streamlined handleUserSelection function with improved error handling and logical separation.
    - Enhanced button collector functionality to update participant statuses and event embeds dynamically.

--- handlers/user-selection-for-invite.js
+++ handlers/user-selection-for-invite.js
@@ -2,148 +2,154 @@ const { EmbedBuilder, ActionRowBuilder, ButtonBuilder } = require('discord.js')
 const { fetchEvent, addOrUpdateParticipant } = require('../services/eventService')
 
 function getMentionUsersString(participantsIds) {
-  const mentionUsers = participantsIds.map((discordID) => `<@${discordID}>`).join(', ')
+  return participantsIds.map((discordID) => `<@${discordID}>`).join(', ')
+}
 
-  return mentionUsers
+async function updateParticipantStatus(eventId, userId, status) {
+  await addOrUpdateParticipant(eventId, userId, status)
+}
+
+async function getUpdatedEvent(eventId) {
+  return await fetchEvent(eventId)
+}
+
+function createEventEmbed(eventId, embedDescription, participants) {
+  const statuses = [
+    { label: 'Attending ✅:', status: 'attending' },
+    { label: 'Declined ❌:', status: 'declined' },
+    { label: 'Considering 🤔:', status: 'considering' },
+    { label: 'Invited - awaiting response 🕐:', status: 'invited' },
+  ]
+
+  let responseText = `Event ID: ${eventId}\n\n`
+
+  statuses.forEach(({ label, status }) => {
+    responseText += `${label}\n`
+    participants
+      .filter((participant) => participant.status === status)
+      .forEach((participant) => {
+        responseText += `<@${participant.discordID}>\n`
+      })
+    responseText += '\n'
+  })
+
+  return new EmbedBuilder()
+    .setTitle('Event Invitation')
+    .setDescription(embedDescription)
+    .addFields({ name: 'Responses', value: responseText })
+}
+
+function createButtons() {
+  return new ActionRowBuilder().addComponents(
+    new ButtonBuilder().setCustomId('attending').setLabel('Attend').setStyle('Success'),
+    new ButtonBuilder().setCustomId('declined').setLabel('Decline').setStyle('Danger'),
+    new ButtonBuilder().setCustomId('considering').setLabel('Considering').setStyle('Secondary')
+  )
 }
 
 const handleUserSelection = async (interaction) => {
   const selectedUsers = interaction.values
   const eventId = interaction.customId.split(':')[1]
-  const selectedEvent = await fetchEvent(eventId)
-
+  let selectedEvent = await getUpdatedEvent(eventId)
   const embedDescription = `You have been invited to the event ${selectedEvent.title} by ${interaction.user}.`
   const participantIDs = selectedEvent.participants.map((participant) => participant.discordID)
   const uniqueUsers = selectedUsers.filter((user) => !participantIDs.includes(user))
 
-  for (const uniqueUser of uniqueUsers) {
-    await addOrUpdateParticipant(eventId, uniqueUser, 'invited')
-  }
+  try {
+    for (const uniqueUser of uniqueUsers) {
+      await updateParticipantStatus(eventId, uniqueUser, 'invited')
+    }
 
-  selectedEvent = await fetchEvent(eventId)
-  participants = selectedEvent.participants
+    selectedEvent = await getUpdatedEvent(eventId)
+    const participants = selectedEvent.participants
+    const matchedParticipants = participants.filter((participant) => selectedUsers.includes(participant.discordID))
+    const allUsersProcessed = matchedParticipants.length === selectedUsers.length
+    const allSelectedUsersHaveResponded = matchedParticipants.every((participant) => participant.status !== 'invited')
 
-  const matchedParticipants = participants.filter((participant) => selectedUsers.includes(participant.discordID))
-  const allUsersProcessed = matchedParticipants.length === selectedUsers.length
-  const allSelectedUsersHaveResponded = matchedParticipants.every((participant) => participant.status !== 'invited')
+    const embed = createEventEmbed(eventId, embedDescription, participants)
+    const buttons = createButtons()
 
-  const buttons = new ActionRowBuilder().addComponents(
-    new ButtonBuilder().setCustomId('attending').setLabel('Attend').setStyle('Success'),
-    new ButtonBuilder().setCustomId('declined').setLabel('Decline').setStyle('Danger'),
-    new ButtonBuilder().setCustomId('considering').setLabel('Considering').setStyle('Secondary')
-  )
-
-  const generateResponseText = () => {
-    const statuses = [
-      { label: 'Attending ✅:', status: 'attending' },
-      { label: 'Declined ❌:', status: 'declined' },
-      { label: 'Considering 🤔:', status: 'considering' },
-      { label: 'Invited - awaiting response 🕐:', status: 'invited' },
-    ]
-
-    let responseText = `Event ID: ${eventId}\n\n`
-
-    statuses.forEach(({ label, status }) => {
-      responseText += `${label}\n`
-      participants
-        .filter((participant) => participant.status === status)
-        .forEach((participant) => {
-          responseText += `<@${participant.discordID}>\n`
-        })
-      responseText += '\n'
-    })
-
-    return responseText
-  }
-
-  const embed = new EmbedBuilder()
-    .setTitle('Event Invitation')
-    .setDescription(embedDescription)
-    .addFields({ name: 'Responses', value: generateResponseText() })
-
-  if (allUsersProcessed && allSelectedUsersHaveResponded) {
-    await interaction.update({
-      content: `All the users you invited have already responded and were not re-invited: ${getMentionUsersString(
-        selectedUsers
-      )}. Please check the list below for the current status.`,
-      embeds: [embed],
-      components: [],
-      ephemeral: true,
-    })
-    return
-  }
-
-  const invitedUsers = participants
-    .filter((participant) => selectedUsers.includes(participant.discordID) && participant.status === 'invited')
-    .map((participant) => participant.discordID)
-
-  const notInvitedUsers = participants
-    .filter((participant) => selectedUsers.includes(participant.discordID) && participant.status !== 'invited')
-    .map((participant) => participant.discordID)
-
-  const inviteMessage = await interaction.channel.send({
-    content: `${getMentionUsersString(invitedUsers)}, ${embedDescription}.`,
-    embeds: [embed],
-    components: [buttons],
-  })
-
-  if (notInvitedUsers.length > 0) {
-    await interaction.reply({
-      content: `The following users have already responded to the invitation and were not re-invited: ${getMentionUsersString(
-        notInvitedUsers
-      )}. Please check the list above for the current status.`,
-      ephemeral: true,
-    })
-  }
-
-  const buttonFilter = (i) => ['attending', 'declined', 'considering'].includes(i.customId)
-
-  const threeDaysInMilliseconds = 3 * 24 * 60 * 60 * 1000
-
-  const buttonCollector = inviteMessage.createMessageComponentCollector({
-    filter: buttonFilter,
-    time: threeDaysInMilliseconds,
-  })
-
-  buttonCollector.on('collect', async (i) => {
-    const currentTime = new Date()
-    const eventStartTime = new Date(selectedEvent.startTime)
-
-    if (currentTime > eventStartTime) {
-      await i.reply({
-        content: 'This event has already started, you can no longer make a decision.',
+    if (allUsersProcessed && allSelectedUsersHaveResponded) {
+      await interaction.update({
+        content: `All the users you invited have already responded and were not re-invited: ${getMentionUsersString(
+          selectedUsers
+        )}. Please check the list below for the current status.`,
+        embeds: [embed],
+        components: [],
         ephemeral: true,
       })
       return
     }
 
-    await addOrUpdateParticipant(eventId, i.user.id, i.customId)
+    const invitedUsers = participants
+      .filter((participant) => selectedUsers.includes(participant.discordID) && participant.status === 'invited')
+      .map((participant) => participant.discordID)
 
-    selectedEvent = await fetchEvent(eventId)
-    participants = selectedEvent.participants
+    const notInvitedUsers = participants
+      .filter((participant) => selectedUsers.includes(participant.discordID) && participant.status !== 'invited')
+      .map((participant) => participant.discordID)
 
-    const updatedEmbed = new EmbedBuilder()
-      .setTitle('Event Invitation')
-      .setDescription(embedDescription)
-      .addFields({ name: 'Responses', value: generateResponseText() })
+    const inviteMessage = await interaction.channel.send({
+      content: `${getMentionUsersString(invitedUsers)}, ${embedDescription}.`,
+      embeds: [embed],
+      components: [buttons],
+    })
 
-    await inviteMessage.edit({ embeds: [updatedEmbed] })
-
-    await i.deferUpdate()
-  })
-
-  buttonCollector.on('end', async (collected) => {
-    console.log(`Collected ${collected.size} button interactions.`)
-    if (participants.unanswered.length > 0) {
-      const newButtons = new ActionRowBuilder().addComponents(
-        new ButtonBuilder().setCustomId('attending').setLabel('Attend').setStyle('Success'),
-        new ButtonBuilder().setCustomId('declined').setLabel('Decline').setStyle('Danger'),
-        new ButtonBuilder().setCustomId('considering').setLabel('Considering').setStyle('Secondary')
-      )
-
-      await inviteMessage.edit({ components: [newButtons] })
+    if (notInvitedUsers.length > 0) {
+      await interaction.reply({
+        content: `The following users have already responded to the invitation and were not re-invited: ${getMentionUsersString(
+          notInvitedUsers
+        )}. Please check the list above for the current status.`,
+        ephemeral: true,
+      })
     }
-  })
+
+    const buttonFilter = (i) => ['attending', 'declined', 'considering'].includes(i.customId)
+
+    const threeDaysInMilliseconds = 3 * 24 * 60 * 60 * 1000
+
+    const buttonCollector = inviteMessage.createMessageComponentCollector({
+      filter: buttonFilter,
+      time: threeDaysInMilliseconds,
+    })
+
+    buttonCollector.on('collect', async (i) => {
+      const currentTime = new Date()
+      const eventStartTime = new Date(selectedEvent.startTime)
+
+      if (currentTime > eventStartTime) {
+        await i.reply({
+          content: 'This event has already started, you can no longer make a decision.',
+          ephemeral: true,
+        })
+        return
+      }
+
+      await updateParticipantStatus(eventId, i.user.id, i.customId)
+      selectedEvent = await getUpdatedEvent(eventId)
+      const participants = selectedEvent.participants
+
+      const updatedEmbed = createEventEmbed(eventId, embedDescription, participants)
+      await inviteMessage.edit({ embeds: [updatedEmbed] })
+      await i.deferUpdate()
+    })
+
+    buttonCollector.on('end', async () => {
+      selectedEvent = await getUpdatedEvent(eventId)
+      const participants = selectedEvent.participants
+
+      if (participants.some((participant) => participant.status === 'invited')) {
+        const newButtons = createButtons()
+        await inviteMessage.edit({ components: [newButtons] })
+      }
+    })
+  } catch (error) {
+    console.error('Error handling user selection:', error)
+    await interaction.reply({
+      content: 'There was an error handling your selection. Please try again later.',
+      ephemeral: true,
+    })
+  }
 }
 
 module.exports = handleUserSelection
