98f75a0527ace8104e67c71d86332d6e882eca41
skyturkish <gokturk.acr2002@gmail.com>
2024-06-19 22:51:38 +0300

    Global command deployment

--- deploy-commands.js
+++ deploy-commands.js
@@ -19,8 +19,8 @@ const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN)
   try {
     console.log(`Started refreshing ${commands.length} application (/) commands.`)
 
-    // Guild command deployment
-    const data = await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), {
+    // Global command deployment
+    const data = await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
       body: commands,
     })
 
