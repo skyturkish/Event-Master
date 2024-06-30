d56ad7cdeb677477fc97af739c81b9c35092417c
skyturkish <gokturk.acr2002@gmail.com>
2024-06-30 10:34:05 +0300

    Create getUsersName

--- utils/mentionUtils.js
+++ utils/mentionUtils.js
@@ -4,6 +4,17 @@ function getMentionUsersString(usersIds) {
   return usersIds.map((discordID) => `<@${discordID}>`).join(', ')
 }
 
+async function getUsersName(usersIds, client) {
+  const users = await Promise.all(
+    usersIds.map(async (discordID) => {
+      const user = await client.users.fetch(discordID)
+      return user.username // Assuming you want to get the username property
+    })
+  )
+  return users.join(', ')
+}
+
 module.exports = {
   getMentionUsersString,
+  getUsersName,
 }
